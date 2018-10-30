2187027d8c30afb6bc27c72c6ef09665133b9f9e
Geoff Rich <geoff.rich@gmail.com>
2018-10-30 15:41:19 +1000

    Fix Ubuntu 18.04 base build.

--- packer/ubuntu1804iso/scripts/cleanup.sh
+++ packer/ubuntu1804iso/scripts/cleanup.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/bash -eu
 
 rm /etc/machine-id
 touch /etc/machine-id
+
+exit 0
--- packer/ubuntu1804iso/scripts/setup.sh
+++ packer/ubuntu1804iso/scripts/setup.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
+#!/bin/bash -eu
 
-apt-get update
+apt-get -y update
 apt-get -y dist-upgrade
+
+# reboot
+echo "====> Shutting down the SSHD service and rebooting..."
+systemctl stop sshd.service
+nohup shutdown -r now < /dev/null > /dev/null 2>&1 &
+sleep 120
+exit 0
