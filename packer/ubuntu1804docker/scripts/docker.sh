00540dd5342d266870997bde10d76a58102a7b39
Geoff Rich <geoff.rich@gmail.com>
2018-11-22 09:59:55 +1000

    Add dns fix to ubuntu 18 04 docker.

--- packer/ubuntu1804docker/scripts/docker.sh
+++ packer/ubuntu1804docker/scripts/docker.sh
@@ -4,7 +4,9 @@ apt-get update
 apt-get install -y apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
+    software-properties-common \
+    dnsmasq \
+    resolvconf
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -15,3 +17,17 @@ apt-get install -y docker-ce
 groupadd docker
 echo "Add $DOCKERUSERNAME to docker group"
 usermod -aG docker $DOCKERUSERNAME
+
+# Fix dns on ubuntu 18.04 for docker containers (requires dnsmasq and resolvconf installed)
+
+# Edit /etc/dnsmsaq.conf and add these lines:
+echo "interface=docker0" >> /etc/dnsmsaq.conf
+echo "bind-interfaces" >> /etc/dnsmsaq.conf
+echo "listen-address=172.17.0.1" >> /etc/dnsmsaq.conf
+
+# Create/edit /etc/resolvconf/resolv.conf.d/tail and add this line:
+echo "nameserver 172.17.0.1" >> /etc/resolvconf/resolv.conf.d/tail
+
+systemctl restart dnsmasq.service
+resolvconf -u
+systemctl restart docker
