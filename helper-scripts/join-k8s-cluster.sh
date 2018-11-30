e4589acf59fd8dc8a2acd3b265ba74907c6bec3f
Geoff Rich <geoff.rich@gmail.com>
2018-11-30 15:33:29 +1000

    More work on k8s scripts.

--- helper-scripts/join-k8s-cluster.sh
+++ helper-scripts/join-k8s-cluster.sh
@@ -3,8 +3,8 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-REMOTE_MASTER_IP=`./get-environment-value.sh masterip`
-SHA_TOKEN=`./get-environment-value.sh shatoken`
-K8S_TOKEN=`./get-environment-value.sh k8stoken`
+REMOTE_MASTER_IP=$(./get-environment-value.sh masterip)
+SHA_TOKEN=$(./get-environment-value.sh shatoken)
+K8S_TOKEN=$(./get-environment-value.sh k8stoken)
 
 kubeadm join $REMOTE_MASTER_IP:6443 --token $K8S_TOKEN --discovery-token-ca-cert-hash sha256:$SHA_TOKEN
--- helper-scripts/setup-k8s-master.sh
+++ helper-scripts/setup-k8s-master.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# Note: this script still needs a reasonable amount of work.
-
 kubeadm init --ignore-preflight-errors=all --cri-socket /run/containerd/containerd.sock --pod-network-cidr=10.1.0.0/16
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
-SHA_TOKEN=`openssl x509 -in /etc/kubernetes/pki/ca.crt -noout -pubkey | openssl rsa -pubin -outform DER 2>/dev/null | sha256sum | cut -d' ' -f1`
-K8S_TOKEN=`kubeadm token list | cut -d' ' -f1 | sed -n 2p`
-MASTER_IP=`ip addr show ens192 | grep -Po 'inet \K[\d.]+'`
+SHA_TOKEN=$(openssl x509 -in /etc/kubernetes/pki/ca.crt -noout -pubkey | openssl rsa -pubin -outform DER 2>/dev/null | sha256sum | cut -d' ' -f1)
+K8S_TOKEN=$(kubeadm token list | cut -d' ' -f1 | sed -n 2p)
+MASTER_IP=$(ip addr show ens192 | grep -Po 'inet \K[\d.]+')
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
