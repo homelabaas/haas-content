184054833bdf0600df5a4c39f7aa295d23571d71
Geoff Rich <geoff.rich@gmail.com>
2019-04-09 17:20:02 +1000

    Add gitattributes.

---
+++ upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mc cp -r helper-scripts/ local/content/helper-scripts
+mc cp -r cloud-configs/ local/content/cloud-configs
+mc cp -r deployment-compose/ local/content/deployment-compose
+mc cp -r environments/ local/content/environments
+mc cp -r packer/ local/content/packer
